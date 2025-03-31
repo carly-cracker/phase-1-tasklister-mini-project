280c165bdc2737683c945965b1aa52f33c2ba89b
Korir Carlos <carlkorir226@gmail.com>
2025-03-31 23:38:18 +0300

    adds some event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const form = document.getElementById("create-task-form")
+  form.addEventListener("submit",handleFormSubmit)
+
+  function handleFormSubmit(event){
+    event.preventDefault()
+    const tasks = document.getElementById("tasks")
+    const newTask = document.getElementById("new-task-description")
+    let taskString = document.createElement("p")
+    console.log("happy")
+    taskString.innerHTML = newTask.value
+    
+    tasks.appendChild(taskString)
+    
+  }
 });
